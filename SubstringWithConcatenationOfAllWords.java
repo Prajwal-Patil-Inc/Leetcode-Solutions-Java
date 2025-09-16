635637318fe9bd6f5d20a2e2ca69e49b1d1c1ece
[Prajwal Patil] <[prajwalpatil.2014@gmail.com]>
2025-09-16 20:34:44 +0100

    Updated comments

--- SubstringWithConcatenationOfAllWords.java
+++ SubstringWithConcatenationOfAllWords.java
@@ -8,7 +8,6 @@ public class SubstringWithConcatenationOfAllWords {
 
         HashMap<String, Integer> wordsMap = new HashMap<>();
         
-
         int wordLength = words[0].length();
         int windowLength = wordLength * words.length;
 
@@ -23,31 +22,43 @@ public class SubstringWithConcatenationOfAllWords {
 
             while (right + wordLength <= s.length()) {
 
+                //Right substring -> ex: bar 
                 String sub = s.substring(right, right + wordLength);
 
+                //If valid string
                 if (wordsMap.containsKey(sub)) {
+                    
+                    //Put in seen
                     seen.put(sub, seen.getOrDefault(sub, 0) + 1);
 
+                    //If the frequency count of the right substring gets more than in the wordsmap
                     if (seen.get(sub) > wordsMap.get(sub)) {
+                        
+                        //Get left substring
                         String leftSub = s.substring(left, left + wordLength);
 
+                        //Shrink till we get equal number of frequencies
                         while (seen.get(leftSub) > wordsMap.get(leftSub)) {
                             seen.put(leftSub, seen.get(leftSub) - 1);
                             left += wordLength;
-                            leftSub = s.substring(left, left + wordLength);
+                            leftSub = s.substring(left, left + wordLength); //Get next substring
                         }
                     }
-                } else {
+                }
+                //If not a valid substring 
+                else {
                     seen.clear();
-                    left = right + wordLength;
+                    left = right + wordLength; //Move left -> shrink by twice 
                 }
 
+                //If window is hit
                 if (right + wordLength - left == windowLength) {
                     if (seen.equals(wordsMap)) {
                         res.add(left);
                     }
                 }
 
+                //Move right
                 right += wordLength;
             }
         }
